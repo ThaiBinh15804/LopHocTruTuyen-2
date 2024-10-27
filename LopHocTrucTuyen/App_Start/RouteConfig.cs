028f9525820dbee1935b48721c8223517d82bf70
DuyKhanh <duykhanh18102004@gmail.com>
2024-10-27 21:03:10 +0700

    Thông điệp commit của bạn

--- LopHocTrucTuyen/App_Start/RouteConfig.cs
+++ LopHocTrucTuyen/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace LopHocTrucTuyen
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "GiangVien", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "HocVien", action = "TrangChu", id = UrlParameter.Optional }
             );
         }
     }
