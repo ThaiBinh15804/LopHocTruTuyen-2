27e740358014cbfa6681bd27e197292429e5a364
DuyKhanh <duykhanh18102004@gmail.com>
2024-10-28 00:06:44 +0700

    Thông điệp commit

---
+++ LopHocTrucTuyen/Controllers/HocVienController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LopHocTrucTuyen.Models;
+
+namespace LopHocTrucTuyen.Controllers
+{
+    public class HocVienController : Controller
+    {
+        //
+        // GET: /HocVien/
+        DataClasses1DataContext db = new DataClasses1DataContext();
+        public ActionResult TrangChu()
+        {
+            List<KhoaHoc> dskh = db.KhoaHocs.ToList();
+            return View(dskh);
+        }
+        public ActionResult HocTap()
+        {
+            return View();
+        }
+        public ActionResult ThongTin()
+        {
+            return View();
+        }
+    }
+}
