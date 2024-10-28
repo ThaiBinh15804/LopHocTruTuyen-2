0603ca4b8f571d0839ef89a4ec53008ee74dd819
DuyKhanh <duykhanh18102004@gmail.com>
2024-10-28 11:41:49 +0700

    Code moi

--- LopHocTrucTuyen/Controllers/HocVienController.cs
+++ LopHocTrucTuyen/Controllers/HocVienController.cs
@@ -3,27 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LopHocTrucTuyen.Filter; // Thêm namespace của bộ lọc
 using LopHocTrucTuyen.Models;
 
 namespace LopHocTrucTuyen.Controllers
 {
+    [YeuCauDangNhap] // Áp dụng bộ lọc cho toàn bộ controller
     public class HocVienController : Controller
     {
-        //
-        // GET: /HocVien/
         DataClasses1DataContext db = new DataClasses1DataContext();
+
         public ActionResult TrangChu()
         {
             List<KhoaHoc> dskh = db.KhoaHocs.ToList();
             return View(dskh);
         }
+
         public ActionResult HocTap()
         {
             return View();
         }
+
         public ActionResult ThongTin()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DangNhap(string username, string password)
+        {
+            var user = db.NguoiDungs.FirstOrDefault(u => u.TenDangNhap == username && u.MatKhau == password);
+            if (user != null)
+            {
+                Session["User"] = user; // Lưu thông tin người dùng vào Session
+                return RedirectToAction("TrangChu", "HocVien");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Tên đăng nhập hoặc mật khẩu không đúng";
+                return View();
+            }
+        }
+
+        public ActionResult DangXuat()
+        {
+            Session.Clear(); // Xóa thông tin Session khi đăng xuất
+            return RedirectToAction("DangNhap");
         }
     }
 }
---
+++ LopHocTrucTuyen/Filter/YeuCauDangNhap.cs
@@ -0,0 +1,31 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace LopHocTrucTuyen.Filter
+{
+    public class YeuCauDangNhap : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            // Kiểm tra nếu người dùng chưa đăng nhập
+            if (HttpContext.Current.Session["User"] == null)
+            {
+                // Lấy tên controller và action hiện tại
+                var controllerName = filterContext.ActionDescriptor.ControllerDescriptor.ControllerName;
+                var actionName = filterContext.ActionDescriptor.ActionName;
+
+                // Nếu không phải là trang DangNhap, thì chuyển hướng đến trang đăng nhập
+                if (!(controllerName == "HocVien" && actionName == "DangNhap"))
+                {
+                    filterContext.Result = new RedirectToRouteResult(
+                        new System.Web.Routing.RouteValueDictionary
+                        {
+                            { "controller", "HocVien" },
+                            { "action", "DangNhap" }
+                        });
+                }
+            }
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
